fb2ee0e3a878d72dd8febe43b9687421e53ba9ec
isomorphica <chevalier.jared@gmail.com>
2019-04-09 15:48:19 -0400

    tutorial

--- Malfunction/Assets/Scripts/TutorialPanel.cs
+++ Malfunction/Assets/Scripts/TutorialPanel.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class TutorialPanel : MonoBehaviour {
 
     public Text mainText;
-    string[] jsonHeaders = { "Tutorial_1", "Tutorial_2", "Tutorial_3","Tutorial_4", "Tutorial_5" };
+    string[] jsonHeaders = { "Tutorial_1", "Tutorial_2", "Tutorial_3","Tutorial_4", "Tutorial_5", "Tutorial_6", "Tutorial_7", "Tutorial_8", "Tutorial_9", "Tutorial_10", "Tutorial_11", "Tutorial_12" };
     int headerIndex = 0;
 
     public void Initialize()
@@ -22,7 +22,7 @@ public class TutorialPanel : MonoBehaviour {
     public void NextButtonPressed()
     {
         headerIndex++;
-        if (headerIndex >= 5)
+        if (headerIndex >= 12)
         {
             EndTutorial();
         }
